fe62c23814b5aa28de4b0a1bc48f01d617f4ecd8
Dan A <danabramsonwriter@gmail.com>
2025-02-16 09:26:05 +1100

    Created new bookmark pop activity.
    Signed-off-by: Dan A <danabramsonwriter@gmail.com>

---
+++ app/src/main/java/com/shakespeare/new_app/NewBookmarkPop.java
@@ -0,0 +1,12 @@
+package com.shakespeare.new_app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NewBookmarkPop extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_bookmark_pop);
+    }
+}

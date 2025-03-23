88989a078808a1489e52ea13bf749c156a592505
Dan Misra <danmisra0@gmail.com>
2025-03-23 15:29:33 +1100

    Updated versionCode to 12 and versionName to 1.22 to allow load to Google Play Developer Console as v12 for closed testing.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.shakespeare.new_app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 11
-        versionName = "1.21"
+        versionCode = 12
+        versionName = "1.22"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

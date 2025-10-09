79e114b3b9bac5ce03e86cc06d87c97d2fe140b0
Dan A <danabramsonwriter@gmail.com>
2025-10-10 07:43:26 +1100

    Updated to Gradle version 8.7.3.
    Signed-off-by: Dan A <danabramsonwriter@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.8.0")
+        classpath("com.android.tools.build:gradle:8.7.3")
+//        classpath("com.android.tools.build:gradle:8.8.0")
 //        classpath("com.android.tools.build:gradle:8.9.1")
         classpath("com.google.gms:google-services:4.4.2") // ✅ Add this line
         classpath("io.github.cdimascio:dotenv-kotlin:6.4.1")

f1a0c16997ba4884937fbea0aa9c62b533cf39ca
Dan Misra <danmisra0@gmail.com>
2025-11-28 22:39:47 +1100

    Added files for improved structure towards giving the user more resources on each respective text.

--- app/src/main/java/com/shakespeare/new_app/MyApplication.java
+++ app/src/main/java/com/shakespeare/new_app/MyApplication.java
@@ -9,43 +9,49 @@ import java.util.Locale;
 import java.util.Set;
 
 public class MyApplication extends Application {
+
     private static final String TAG = "MyApplication";
+
+    // 🔹 Global singleton instance
+    private static MyApplication instance;
+
+    // 🔹 Existing TTS field
     public static TextToSpeech textToSpeech;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        textToSpeech = new TextToSpeech(this
-                ,
-                new TextToSpeech.OnInitListener() {
-                    @Override
-                    public void onInit(int i) {
-                      /*  Set<Voice> voices = getVoices();
-                        for (Voice voice : voices) {
 
-                            Log.d(TAG, "setLanguage: "+voice.getName());
-                            for (String feature : voice.getFeatures()) {
-                                Log.d(TAG, "setLanguage:feature "+feature);
-                            }
+        // 🔹 Initialise singleton
+        instance = this;
 
-                        }*/
-                        if (i == TextToSpeech.SUCCESS) {
-                            Log.d(TAG, "onInit: init success");
-                        }else {
-                            Log.d(TAG, "onInit: init failed!");
-                        }
-                    }
-                }/*,"com.google.android.tts"*/);
+        // 🔹 Existing TTS initialisation
+        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                if (i == TextToSpeech.SUCCESS) {
+                    Log.d(TAG, "onInit: init success");
+                } else {
+                    Log.d(TAG, "onInit: init failed!");
+                }
+            }
+        });
     }
 
-    public  static Set<Voice> getVoices(){
-      return  textToSpeech.getVoices();
+    // 🔹 Global accessor for Application context
+    public static MyApplication getInstance() {
+        return instance;
     }
 
-    public static void setLanguage(Locale lang){
-      if (textToSpeech!=null && textToSpeech.isLanguageAvailable(lang)!=TextToSpeech.LANG_NOT_SUPPORTED){
-          textToSpeech.setLanguage(lang);
-      }
+    // 🔹 Existing helper methods (unchanged)
+    public static Set<Voice> getVoices() {
+        return textToSpeech.getVoices();
     }
 
+    public static void setLanguage(Locale lang) {
+        if (textToSpeech != null &&
+                textToSpeech.isLanguageAvailable(lang) != TextToSpeech.LANG_NOT_SUPPORTED) {
+            textToSpeech.setLanguage(lang);
+        }
+    }
 }
